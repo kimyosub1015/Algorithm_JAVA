3734f3cd0d2c9a99925a1d442ad304178d6c8f1b
dytjq <dytjq@DESKTOP-AH7JKUQ>
2018-10-09 19:03:31 +0900

    quest3 and resive quest2

--- src/Part1/quest3.java
+++ src/Part1/quest3.java
@@ -1,18 +1,20 @@
 package Part1;
 
-public class quest2 {
-	static int min3(int a,int b,int c) {
+public class quest3 {
+	static int min4(int a,int b,int c,int d) {
 		int min = a;
 		if(b<min)
 			min = b;
 		if(c<min)
 			min = c;
+		if(d<min)
+			min = d;
 		return min;
 		
 	}
 
 	public static void main(String[] args) {
-		System.out.println("최소값 : "+min3(11,40,38));
+		System.out.println("최소값 : "+min4(33,40,38,59));
 	
 	}
 
