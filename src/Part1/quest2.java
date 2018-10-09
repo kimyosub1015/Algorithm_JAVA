09a74e82ab480bc08072d75d687dce98503c5b1f
dytjq <dytjq@DESKTOP-AH7JKUQ>
2018-10-09 18:59:56 +0900

    quest2

---
+++ src/Part1/quest2.java
@@ -0,0 +1,19 @@
+package Part1;
+
+public class quest2 {
+	static int min3(int a,int b,int c) {
+		int min = a;
+		if(b<min)
+			min = b;
+		if(c<min)
+			min = c;
+		return min;
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("최대값 : "+min3(11,40,38));
+	
+	}
+
+}
